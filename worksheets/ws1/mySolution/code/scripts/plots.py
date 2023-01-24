534c980b8c41dc2ae31e09d3e6b17efb2c43ac57
Joaquin Rapela <joacorapela@gmail.com>
2023-01-24 17:35:36 +0000

    Renamed p_values to p_values_hist

--- worksheets/ws1/mySolution/code/scripts/plots.py
+++ worksheets/ws1/mySolution/code/scripts/plots.py
@@ -2,13 +2,13 @@
 import plotly.graph_objects as go
 
 
-def getPlotHistPValues(bin_centers, p_values, title, errors=None):
+def getPlotHistPValues(bins_centers, p_values_hist, title, errors=None):
     fig = go.Figure()
     if errors is not None:
-        trace = go.Bar(x=bin_centers, y=p_values,
+        trace = go.Bar(x=bins_centers, y=p_values_hist,
                        error_y=dict(type="data", array=errors))
     else:
-        trace = go.Bar(x=bin_centers, y=p_values)
+        trace = go.Bar(x=bins_centers, y=p_values_hist)
     fig.add_trace(trace)
     fig.update_layout(xaxis_title="p value", yaxis_title="count", title=title)
     return fig

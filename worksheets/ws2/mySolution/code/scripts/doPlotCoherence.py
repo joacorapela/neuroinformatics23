2008b54374b7eed11dc3c90bd5cb2fcd01013ca7
Joaquin Rapela <joacorapela@gmail.com>
2023-02-16 11:40:20 +0000

    Added timing info

--- worksheets/ws2/mySolution/code/scripts/doPlotCoherence.py
+++ worksheets/ws2/mySolution/code/scripts/doPlotCoherence.py
@@ -1,5 +1,6 @@
 
 import sys
+import time
 import argparse
 import numpy as np
 import scipy.signal
@@ -24,6 +25,7 @@ def main(argv):
                         help="plotting start time (sec)", default=0.0)
     parser.add_argument("--duration", type=float,
                         help="signal duration (sec)", default=10.0)
+                        # help="signal duration (sec)", default=3.0)
     parser.add_argument("--segment_len", type=int,
                         help="Segement length (samples)", default=-1)
     parser.add_argument("--n_ticks", type=int,
@@ -36,7 +38,7 @@ def main(argv):
                         default="Channel Number")
     parser.add_argument("--fig_filename_pattern", type=str,
                         help="figure filename pattern",
-                        default=("../../figures/coherence_startTime_{:.02f}_"
+                        default=("../../figures/coh_startTime_{:.02f}_"
                                  "duration_{:.02f}_segmentLen_{:d}_"
                                  "pid_{:s}_channel_{:d}.{:s}"))
     args = parser.parse_args()
@@ -45,37 +47,60 @@ def main(argv):
     channel_nro = args.channel_nro
     start_time = args.start_time
     duration = args.duration
-    segment_len = args.segment_len if args.segment_len > 0 else None
+    segment_len = args.segment_len
     n_ticks = args.n_ticks
     colorscale = args.colorscale
     x_label = args.x_label
     y_label = args.y_label
     fig_filename_pattern = args.fig_filename_pattern
 
+    print("Connecting to IBL ...")
+    t0 = time.time()
     aOne = one.api.ONE(base_url="https://openalyx.internationalbrainlab.org",
                        password="international", silent=True)
     sr = brainbox.io.spikeglx.Streamer(pid=pid, one=aOne, remove_cached=False,
                                        typ="lf")
 
+    # extract channel location acronyms for hover
+    eID, probe_label = aOne.pid2eid(pid=pid)
+    els = brainbox.io.one.load_channel_locations(eID, one=aOne)
+    channel_locs_acronyms = els[probe_label]["acronym"]
+
     s0 = start_time * sr.fs
     tsel = slice(int(s0), int(s0) + int(duration * sr.fs))
+    elapsed_time = time.time() - t0
+    print(f"elapsed_time: {elapsed_time}")
 
-    lfp = sr[tsel, :]*1000
+    print("Getting LFP ...")
+    t0 = time.time()
+
+    lfp = sr[tsel, :-sr.nsync]*1000
     n_samples, n_channels = lfp.shape
     channels = np.arange(n_channels)
     print(f"Data has {n_samples} samples and {n_channels} channels")
+    elapsed_time = time.time() - t0
+    print(f"elapsed_time: {elapsed_time}")
 
-    f, coh = scipy.signal.coherence(x=lfp[tsel, channel_nro],
-                                    y=lfp[tsel, channels], fs=sr.fs,
-                                    nperseg=segment_len)
+    print("Computing coherence ...")
+    t0 = time.time()
+    nperseg = segment_len if segment_len>0 else None
+    f, coh = scipy.signal.coherence(
+        x=lfp[tsel, channel_nro], y=lfp[tsel, channels].T, fs=sr.fs,
+        nperseg=nperseg)
+    elapsed_time = time.time() - t0
+    print(f"elapsed_time: {elapsed_time}")
 
     # let's plot now
+    print("Plotting ...")
+    t0 = time.time()
     hovertext = []
     for yi, yy in enumerate(channels):
         hovertext.append([])
         for xi, xx in enumerate(f):
-            hovertext[-1].append(f"freq: {xx}<br />channel: {yy}<br />"
-                                 f"coh: {coh[yi][xi]}<br />")
+            hovertext[-1].append(f"freq: {xx}<br />"
+                                 f"channel: {yy}<br />"
+                                 f"coh: {coh[yi][xi]}<br />"
+                                 f"loc: {channel_locs_acronyms[yi]}")
 
     zmin, zmax = 0.0, 1.0
 
@@ -84,21 +109,22 @@ def main(argv):
     fig = go.Figure()
     trace = go.Heatmap(x=f, y=channels, z=coh, colorscale=colorscale,
                        zmin=zmin, zmax=zmax,
-                       colorbar=plotUtils.colorbar(zmin=zmin, zmax=zmax,
-                                                   n_ticks=n_ticks,
-                                                   title="coh"))
+                       hoverinfo="text", text=hovertext,
+                      )
     fig.add_trace(trace)
     fig.update_xaxes(title_text=x_label)
     fig.update_yaxes(title_text=y_label)
     fig.update_layout(title=title)
+    elapsed_time = time.time() - t0
+    print(f"elapsed_time: {elapsed_time}")
 
     fig.write_html(fig_filename_pattern.format(start_time, duration,
                                                segment_len, pid, channel_nro,
                                                "html"))
 
-    fig.show()
+    # fig.show()
 
-    breakpoint()
+    # breakpoint()
 
 
 if __name__ == "__main__":

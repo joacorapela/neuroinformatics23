cf7afb5c551860ec1d1d332fd017bed74125a2dc
Joaquin Rapela <joacorapela@gmail.com>
2023-03-01 21:16:50 +0000

    Added kenneth_statistic, kenneth_statistic_modified

--- worksheets/ws4/mySolution/code/scripts/utils.py
+++ worksheets/ws4/mySolution/code/scripts/utils.py
@@ -22,8 +22,30 @@ def lwll_statistic(data, test_xs, fit_func, weight_func, weight_func_scale,
         indices = np.nonzero(np.abs(test_x-data[:, 0]) < x_dist_thr)[0]
         close_data = data[indices, 1]
         fitted_kappa, fitted_theta0, _ = fit_func(close_data)
+        print(f"x; {test_x}, theta: {fitted_theta0}, kappa: {fitted_kappa}, "
+              f"nNeighbors: {len(close_data)}")
         params_x = (fitted_theta0, fitted_kappa)
         answer += locallyWeightedLogLikelihood(
             weight_func=weight_func, weight_func_scale=weight_func_scale,
             logpdf=logpdf, x=test_x, params_x=params_x, data=data)
     return answer
+
+
+def kenneth_statistic(x, theta):
+    indices = np.argsort(x)
+    sorted_theta = theta[indices]
+    sorted_theta_diff = np.diff(sorted_theta)
+    stat = np.sum(np.cos(sorted_theta_diff))
+    return stat
+
+
+def kenneth_statistic_modified(x, theta):
+    indices = np.argsort(x)
+    sorted_theta = theta[indices]
+    distance = 0
+    for i in range(len(sorted_theta)-1):
+        value = 1-np.abs((np.exp(1j*sorted_theta[i+1]) +
+                          np.exp(1j*sorted_theta[i]))/2)
+        distance += value
+        breakpoint()
+    return distance

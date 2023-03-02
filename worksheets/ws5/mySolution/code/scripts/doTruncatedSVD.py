eb06b5335133f5c0c6b5a7b44088150012770dce
Joaquin Rapela <joacorapela@gmail.com>
2023-03-02 18:28:44 +0000

    Substantially improved

--- worksheets/ws5/mySolution/code/scripts/doTruncatedSVD.py
+++ worksheets/ws5/mySolution/code/scripts/doTruncatedSVD.py
@@ -3,14 +3,23 @@ import argparse
 import numpy as np
 import scipy.stats
 import plotly.graph_objs as go
-import matplotlib.pyplot as plt
 
 import one.api
+import brainbox.io.one
+
+import utils
+
+
+def truncate_svd(u, s, vh, n_components):
+    truncated_svd = np.zeros((u.shape[0], vh.shape[1]), dtype=u.dtype)
+    for i in range(n_components):
+        truncated_svd += s[i]*np.outer(u[:, i], vh[i, :])
+    return truncated_svd
 
 
 def main(argv):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--n_svd_components", type=int,
+    parser.add_argument("--n_components", type=int,
                         help="number of SVD components to keep", default=1)
     parser.add_argument("--bin_size", type=float, help="bin_size", default=1.0)
     parser.add_argument("--experiment_id", type=str,
@@ -19,43 +28,31 @@ def main(argv):
     parser.add_argument("--probe_id", type=str,
                         help="id of the probe to analyze",
                         default="probe00")
-    parser.add_argument("--colorscale", type=str, help="colorscale",
-                        default="viridis")
     parser.add_argument("--x_label", type=str, help="x_label",
                         default="Time (sec)")
     parser.add_argument("--y_label", type=str, help="x_label",
                         default="Cluster ID")
-    parser.add_argument("--fig_width", type=int, help="figure width",
-                        default=16)
-    parser.add_argument("--fig_height", type=int, help="figure height",
-                        default=5)
-    parser.add_argument("--zmin", type=float, help="min value to plot on z axis",
-                        default=0.0)
-    parser.add_argument("--zmax", type=int, help="max value to plot on z axis",
-                        default=5.0)
     parser.add_argument("--fig_filename_pattern", type=str,
                         help="figure filename pattern",
-                        default="../../figures/spikes_counts_svd_binSize_{:.02f}_{:s}.{:s}")
+                        default=("../../figures/spikes_counts_truncatedSVD_"
+                                 "binSize_{:.02f}_nComponetns_{:d}.{:s}"))
     args = parser.parse_args()
 
+    n_components = args.n_components
     bin_size = args.bin_size
     eID = args.experiment_id
     probe_id = args.probe_id
-    colorscale = args.colorscale
     x_label = args.x_label
     y_label = args.y_label
     x_label = args.x_label
     y_label = args.y_label
-    fig_width = args.fig_width
-    fig_height = args.fig_height
-    zmin = args.zmin
-    zmax = args.zmax
     fig_filename_pattern = args.fig_filename_pattern
 
     aOne = one.api.ONE(base_url='https://openalyx.internationalbrainlab.org',
                        password='international', silent=True)
     spikes = aOne.load_object(eID, 'spikes', f'alf/{probe_id}/pykilosort')
-    trials = aOne.load_object(eID, 'trials')
+    clusters = aOne.load_object(eID, "clusters", f"alf/{probe_id}/pykilosort")
+    els = brainbox.io.one.load_channel_locations(eID, one=aOne)
 
     spike_time_binary = np.floor(spikes.times/bin_size).astype(int)
     activity_array, edges = np.histogramdd(
@@ -70,50 +67,23 @@ def main(argv):
 
     argsort_res = np.argsort(vh[0, :])
 
-    activity_arrayZ_truncated = truncate_svd(u=u, s=s, vh=vh,
-                                             n_components=n_components)
+    z_truncated = truncate_svd(u=u, s=s, vh=vh[:, argsort_res],
+                               n_components=n_components)
 
-    plt.figure(figsize=(fig_width, fig_height))
-    shw = plt.pcolor(X, Y, activity_arrayZ_truncated[:, argsort_res].T, norm=norm)
-    bar = plt.colorbar(shw)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    bar.set_label("z score")
-    plt.savefig(fig_filename_pattern.format(bin_size, "vh0Sorted",  "png"))
-    # plt.show()
+    zmin, zmax = np.percentile(activity_arrayZ, q=(1.0, 99.0))
 
-    fig = go.Figure()
-    trace = go.Scatter(x=times, y=u[:, 0])
-    fig.add_trace(trace)
-    for i, response_time in enumerate(trials.response_times):
-        fig.add_vline(x=response_time)
-    fig.update_xaxes(title_text=x_label)
-    fig.update_yaxes(title_text="z-score")
-    fig.write_image(fig_filename_pattern.format(bin_size, "uFirstCol",  "png"))
-    fig.write_html(fig_filename_pattern.format(bin_size, "uFirstCol", "html"))
-    # fig.show()
+    hovertext = utils.getHovertext(
+        times=times, clusters_ids=clusters_ids, z=z_truncated.T,
+        channels_for_clusters=clusters.channels,
+        regions_for_channels=els[probe_id]["acronym"])
 
-    extent = (times.min(), times.max(), clusters_ids.min(), clusters_ids.max())
-    argsort_res = np.argsort(vh[0, :])
+    fig = utils.getHeatmap(xs=times, ys=clusters_ids, zs=z_truncated.T,
+                           hovertext=hovertext, zmin=zmin, zmax=zmax,
+                           x_label=x_label, y_label=y_label)
+    fig.write_image(fig_filename_pattern.format(bin_size, n_components, "png"))
+    fig.write_html(fig_filename_pattern.format(bin_size, n_components, "html"))
 
-    plt.figure(figsize=(fig_width, fig_height))
-    plt.imshow(activity_arrayZ[:, argsort_res].T, aspect='auto',
-               vmin=zmin, vmax=zmax, extent=extent)
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
-    plt.savefig(fig_filename_pattern.format(bin_size, "vh0Sorted",  "png"))
-    # plt.show()
-
-    fig = go.Figure()
-    trace = go.Histogram(x=vh[0, :])
-    fig.add_trace(trace)
-    fig.update_xaxes(title_text="weight")
-    fig.update_yaxes(title_text="counts")
-    fig.write_image(fig_filename_pattern.format(bin_size, "uFirstColWeigth",  "png"))
-    fig.write_html(fig_filename_pattern.format(bin_size, "uFirstColWeight", "html"))
-    # fig.show()
-
-    breakpoint()
+    # breakpoint()
 
 
 if __name__ == "__main__":

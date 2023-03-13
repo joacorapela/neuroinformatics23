46b4b39f2faaddd77b0eec23bed9f41056421e1f
Joaquin Rapela <joacorapela@gmail.com>
2023-03-13 20:48:47 +0000

    A few relevant changes

--- worksheets/ws7/mySolution/code/scripts/doComputeOneSefOfPredictions.py
+++ worksheets/ws7/mySolution/code/scripts/doComputeOneSefOfPredictions.py
@@ -5,6 +5,7 @@ import numpy as np
 import sklearn.linear_model
 import plotly.graph_objects as go
 
+import utils
 
 def main(argv):
     parser = argparse.ArgumentParser()
@@ -14,6 +15,9 @@ def main(argv):
                         default=30)
     parser.add_argument("--target_neuron_index", type=int,
                         help="target neuron index", default=29)
+    parser.add_argument("--predict_on_test_data", action="store_true",
+                        help=("use this flag to run the predictions on test "
+                              "data"))
     parser.add_argument("--glm_family", type=str,
                         help="distribution family used in GLM",
                         default="Poisson")
@@ -24,12 +28,13 @@ def main(argv):
     parser.add_argument("--fig_filename_pattern", type=str,
                         help="figure filename pattern",
                         default=("../../figures/predicted_counts_"
-                                 "{:s}_nNeurons_{:d}_nTrials_{:d}.{:s}"))
+                                 "{:s}_predictOnTest_{:d}_nNeurons_{:d}_nTrials_{:d}.{:s}"))
     args = parser.parse_args()
 
     n_trials = args.n_trials
     n_neurons = args.n_neurons
     target_neuron_index = args.target_neuron_index
+    predict_on_test_data = args.predict_on_test_data 
     glm_family = args.glm_family
     data_filename = args.data_filename_pattern.format(n_neurons, n_trials)
     fig_filename_pattern = args.fig_filename_pattern
@@ -45,8 +50,16 @@ def main(argv):
     X = count_data[:, neurons_indices != target_neuron_index]
     y = count_data[:, target_neuron_index]
     model.fit(X=X, y=y)
+    if predict_on_test_data:
+        count_data, trials_weights, neurons_weigts = \
+            utils.simulatePoissonCountsInTrials(n_trials=n_trials,
+                                                n_neurons=n_neurons)
+        X = count_data[:, neurons_indices != target_neuron_index]
+        y = count_data[:, target_neuron_index]
     y_pred = model.predict(X)
+    mse = np.mean((y-y_pred)**2)
     mean_pred_data = {i: y_pred[y == i].mean() for i in np.unique(y)}
+
     fig = go.Figure()
     trace = go.Scatter(x=y, y=y_pred, mode="markers", name="predictions")
     fig.add_trace(trace)
@@ -61,10 +74,13 @@ def main(argv):
 
     fig.update_xaxes(title_text="Observed Spike Count")
     fig.update_yaxes(title_text="Predicted Spike Count")
-    fig.write_image(fig_filename_pattern.format(glm_family, n_neurons,
-                                                n_trials, "png"))
-    fig.write_html(fig_filename_pattern.format(glm_family, n_neurons,
-                                               n_trials, "html"))
+    fig.update_layout(title=f"Mean squared error: {mse:.02f}")
+    fig.write_image(fig_filename_pattern.format(glm_family,
+                                                predict_on_test_data,
+                                                n_neurons, n_trials, "png"))
+    fig.write_html(fig_filename_pattern.format(glm_family,
+                                               predict_on_test_data,
+                                               n_neurons, n_trials, "html"))
 
     breakpoint()
 
